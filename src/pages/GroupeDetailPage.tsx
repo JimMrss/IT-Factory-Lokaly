0f23d8b15ecb1debca3403f569a8331e43a9a396
yohanness <118851598+yohanness29@users.noreply.github.com>
2026-05-20 11:50:38 +0200

    Core(Auth): Ajout logique auth

--- src/pages/GroupeDetailPage.tsx
+++ src/pages/GroupeDetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Button } from '../components/Button';
 import { Badge } from '../components/Badge';
